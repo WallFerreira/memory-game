409ad95dcb1f290784a66be5d2ed9d6a7794e090
WallFerreira <mr.wallaceferreira@gmail.com>
2023-12-04 16:28:50 -0300

    Ajustes finais e finalização do projeto

--- js/game.js
+++ js/game.js
@@ -1,4 +1,6 @@
 const grid = document.querySelector('.grid');
+const spanPlayer = document.querySelector('.player')
+const timer = document.querySelector('.timer');
 
 const characters = [
     'beth',
@@ -20,7 +22,8 @@ const checkEndGame = () => {
     const disabledCards = document.querySelectorAll('.disabled-card')
 
     if (disabledCards.length === 20) {
-        alert('Parabéns Você venceu .. IDIOTA!');
+        clearInterval(this.loop);
+        alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi : ${timer.innerHTML}`);
     };
 }
 
@@ -111,5 +114,18 @@ const loadGame = () => {
     });
 }
 
+const startTimer = () => {
 
-loadGame();
+    this.loop = setInterval(() => {
+        const currentTime = +timer.innerHTML;
+        timer.innerHTML = currentTime + 1;
+    }, 1000);
+}
+
+window.onload = () => {
+
+    spanPlayer.innerHTML = localStorage.getItem('player');
+    startTimer();
+    loadGame();
+}
+
